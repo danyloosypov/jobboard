15575e2c50c2d3ffbea86053fe6c65f1e8421049
danylosant <do@digiants.com.ua>
2025-03-12 20:59:40 +0200

    first commit

---
+++ app/FastAdminPanel/Services/Api/CustomFilter.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\FastAdminPanel\Services\Api;
+
+class CustomFilter
+{
+	public function filter($entity, $method, $crud)
+	{
+		$method = ucfirst($method);
+		$tableName = ucfirst($crud->table_name);
+
+		$filterClass = "\App\FastAdminPanel\Api\Filter\\{$method}{$tableName}Filter";
+
+		if (!class_exists($filterClass)) {
+
+			return;
+		}
+
+		$filter = app()->make($filterClass);
+
+		$filter->change($entity, $crud);
+	}
+}
